539cdac294ae91b7ced6c9c9d30334574a78aff6
Crazy凡 <827799383@qq.com>
2021-11-04 01:01:22 +0800

    MD2 MD4 MD5 SHA1 SHA224 SHA256 SHA384 SHA512 & Test success

--- Sources/FileHash/FileHash.swift
+++ Sources/FileHash/FileHash.swift
@@ -1,6 +1,164 @@
-public struct FileHash {
-    public private(set) var text = "Hello, World!"
+import CommonCrypto
+import CoreFoundation
+import Foundation
 
-    public init() {
+public enum FileHash {
+    public static var defaultChunkSizeForReadingData: Int = 4096
+
+    private static func hashOfFile<T>(atPath filePath: String?, with context: inout Context<T>) -> String? {
+        guard let filePath = filePath else { return nil }
+
+        let url = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, filePath as CFString, .cfurlposixPathStyle, false)
+        let readStream = CFReadStreamCreateWithFile(kCFAllocatorDefault, url)
+
+        var didSucceed = CFReadStreamOpen(readStream)
+
+        guard didSucceed else { return nil }
+
+        var hashObject = context.objectMaker()
+        _ = context.initMethod(&hashObject)
+        var hasMoreData = true
+        var buffer = [UInt8](repeating: 0, count: defaultChunkSizeForReadingData)
+
+        while hasMoreData {
+            let readBytesCount = CFReadStreamRead(readStream, &buffer, defaultChunkSizeForReadingData)
+
+            if readBytesCount == -1 {
+                break
+            } else if readBytesCount == 0 {
+                hasMoreData = false
+            } else {
+                _ = context.updateMethod(&hashObject, &buffer, CC_LONG(readBytesCount))
+            }
+        }
+        CFReadStreamClose(readStream);
+
+        var digest = [UInt8](repeating: 0, count: context.digestLength)
+        _ = context.finalMethod(&digest, &hashObject)
+
+        var result: String = ""
+        didSucceed = !hasMoreData
+
+        if didSucceed {
+            for index in 0..<context.digestLength {
+                result += String(format: "%02x", digest[index])
+            }
+        } else {
+            return nil
+        }
+
+        return result
+    }
+
+    public static func md2HashOfFile(atPath filePath: String?) -> String? {
+        var context = Context<CC_MD2_CTX>(
+            objectMaker: { CC_MD2_CTX() },
+            initMethod: CC_MD2_Init,
+            updateMethod: CC_MD2_Update,
+            finalMethod: CC_MD2_Final,
+            digestLength: 16
+        )
+
+        return hashOfFile(atPath: filePath, with: &context)
+    }
+
+    public static func md4HashOfFile(atPath filePath: String?) -> String? {
+        var context = Context<CC_MD4_CTX>(
+            objectMaker: { CC_MD4_CTX() },
+            initMethod: CC_MD4_Init,
+            updateMethod: CC_MD4_Update,
+            finalMethod: CC_MD4_Final,
+            digestLength: 16
+        )
+
+        return hashOfFile(atPath: filePath, with: &context)
+    }
+
+    public static func md5HashOfFile(atPath filePath: String?) -> String? {
+        var context = Context<CC_MD5_CTX>(
+            objectMaker: { CC_MD5_CTX() },
+            initMethod: CC_MD5_Init,
+            updateMethod: CC_MD5_Update,
+            finalMethod: CC_MD5_Final,
+            digestLength: 16
+        )
+
+        return hashOfFile(atPath: filePath, with: &context)
+    }
+
+    public static func sha1HashOfFile(atPath filePath: String?) -> String? {
+        var context = Context<CC_SHA1_CTX>(
+            objectMaker: { CC_SHA1_CTX() },
+            initMethod: CC_SHA1_Init,
+            updateMethod: CC_SHA1_Update,
+            finalMethod: CC_SHA1_Final,
+            digestLength: 20
+        )
+
+        return hashOfFile(atPath: filePath, with: &context)
+    }
+
+    public static func sha224HashOfFile(atPath filePath: String?) -> String? {
+        var context = Context<CC_SHA256_CTX>(
+            objectMaker: { CC_SHA256_CTX() },
+            initMethod: CC_SHA224_Init,
+            updateMethod: CC_SHA224_Update,
+            finalMethod: CC_SHA224_Final,
+            digestLength: 28
+        )
+
+        return hashOfFile(atPath: filePath, with: &context)
+    }
+
+    public static func sha256HashOfFile(atPath filePath: String?) -> String? {
+        var context = Context<CC_SHA256_CTX>(
+            objectMaker: { CC_SHA256_CTX() },
+            initMethod: CC_SHA256_Init,
+            updateMethod: CC_SHA256_Update,
+            finalMethod: CC_SHA256_Final,
+            digestLength: 32
+        )
+
+        return hashOfFile(atPath: filePath, with: &context)
+    }
+
+    public static func sha384HashOfFile(atPath filePath: String?) -> String? {
+        var context = Context<CC_SHA512_CTX>(
+            objectMaker: { CC_SHA512_CTX() },
+            initMethod: CC_SHA384_Init,
+            updateMethod: CC_SHA384_Update,
+            finalMethod: CC_SHA384_Final,
+            digestLength: 48
+        )
+
+        return hashOfFile(atPath: filePath, with: &context)
+    }
+
+    public static func sha512HashOfFile(atPath filePath: String?) -> String? {
+        var context = Context<CC_SHA512_CTX>(
+            objectMaker: { CC_SHA512_CTX() },
+            initMethod: CC_SHA512_Init,
+            updateMethod: CC_SHA512_Update,
+            finalMethod: CC_SHA512_Final,
+            digestLength: 64
+        )
+
+        return hashOfFile(atPath: filePath, with: &context)
+    }
+}
+
+private extension FileHash {
+    struct Context<HashObject> {
+        typealias ObjectMaker = () -> HashObject
+        typealias InitMethod = (UnsafeMutablePointer<HashObject>?) -> Int32
+        typealias UpdateMethod = (UnsafeMutablePointer<HashObject>?, UnsafeRawPointer?, CC_LONG) -> Int32
+        typealias FinalMethod = (UnsafeMutablePointer<UInt8>?, UnsafeMutablePointer<HashObject>?) -> Int32
+
+        var objectMaker: ObjectMaker
+        var initMethod: InitMethod
+        var updateMethod: UpdateMethod
+        var finalMethod: FinalMethod
+
+        var digestLength: Int
     }
 }
--- Tests/FileHashTests/FileHashTests.swift
+++ Tests/FileHashTests/FileHashTests.swift
@@ -2,10 +2,86 @@ import XCTest
 @testable import FileHash
 
 final class FileHashTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FileHash().text, "Hello, World!")
+    private var cacheFileURL: URL?
+
+    override func setUp() {
+        super.setUp()
+
+        guard let cachePath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else {
+            XCTAssertTrue(false, "Read cache dir failed.")
+            return
+        }
+
+        let fileRUL = URL(fileURLWithPath: cachePath).appendingPathComponent("FileHashTestData.txt")
+        try? FileManager.default.removeItem(atPath: fileRUL.path)
+
+        do {
+            try "FileHashTestData".write(toFile: fileRUL.path, atomically: true, encoding: .utf8)
+        } catch {
+            XCTAssertTrue(false, "Write test data failed.")
+        }
+
+        cacheFileURL = fileRUL
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        if let cacheFileURL = cacheFileURL {
+            do {
+                try FileManager.default.removeItem(atPath: cacheFileURL.path)
+            } catch {
+                XCTAssertTrue(false, "Remove test file error.")
+            }
+        }
+    }
+
+    func testMD2() {
+        XCTAssertEqual(FileHash.md2HashOfFile(atPath: cacheFileURL?.path),
+                       "da979acd488aadbe44085b68bf56be97")
+        XCTAssertNil(FileHash.md2HashOfFile(atPath: nil))
+    }
+
+    func testMD4() {
+        XCTAssertEqual(FileHash.md4HashOfFile(atPath: cacheFileURL?.path),
+                       "da622ec461bd3e8e8590a49845b1f393")
+        XCTAssertNil(FileHash.md4HashOfFile(atPath: nil))
+    }
+
+    func testMD5() {
+        XCTAssertEqual(FileHash.md5HashOfFile(atPath: cacheFileURL?.path),
+                       "6069d978fa7af33751c08654903b5516")
+        XCTAssertNil(FileHash.md5HashOfFile(atPath: nil))
+    }
+
+    func testSHA1() {
+        XCTAssertEqual(
+            FileHash.sha1HashOfFile(atPath: cacheFileURL?.path),
+            "27c38e2a73e30a7b393a5b1cb15d9cbf36394bff")
+        XCTAssertNil(FileHash.sha1HashOfFile(atPath: nil))
+    }
+
+    func testSHA224() {
+        XCTAssertEqual(FileHash.sha224HashOfFile(atPath: cacheFileURL?.path),
+                       "c080cdccc5b4979fee52f175b1ebec3ce88361e7ea1f7d88b798e2d4")
+        XCTAssertNil(FileHash.sha224HashOfFile(atPath: nil))
+    }
+
+    func testSHA256() {
+        XCTAssertEqual(FileHash.sha256HashOfFile(atPath: cacheFileURL?.path),
+                       "f74c815a5b879306b5008a22f2d9b1c9a92014e5d0a8c25cba12f9b8fcd7c726")
+        XCTAssertNil(FileHash.sha256HashOfFile(atPath: nil))
+    }
+
+    func testSHA384() {
+        XCTAssertEqual(FileHash.sha384HashOfFile(atPath: cacheFileURL?.path),
+                       "55d3e8336b58b420e9981062f6b7f57d105b91b98f549379836757795ff8e1fdd365c56732d6deee0be89d58a6c98d3a")
+        XCTAssertNil(FileHash.sha384HashOfFile(atPath: nil))
+    }
+
+    func testSHA512() {
+        XCTAssertEqual(FileHash.sha512HashOfFile(atPath: cacheFileURL?.path),
+                       "eef2b3c08cee9680f72a499d88e60d0547fa87e0f5a8465fa7b0314f85dee6da02b5ef530cae071690b7d3552f20dfedd3aceb3e937bf606bb1e295b00dc27d7")
+        XCTAssertNil(FileHash.sha512HashOfFile(atPath: nil))
     }
 }
